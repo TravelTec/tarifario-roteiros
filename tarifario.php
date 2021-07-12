8ce1565739b17d070f1e091169e006ed54d74bd1
TravelTec <59958315+TravelTec@users.noreply.github.com>
2021-07-12 11:02:42 -0300

    Add files via upload

---
+++ tarifario.php
@@ -0,0 +1,86 @@
+<?php  
+
+
+
+/*
+
+
+
+Plugin Name: Voucher Tec - Tarifário
+
+
+
+Plugin URI: https://traveltec.com.br
+
+
+
+GitHub Plugin URI: https://traveltec.com.br
+
+
+
+Description: Voucher Tec - Tarifário permite o cadastro de tarifários de hotéis de forma manual.
+
+
+ 
+Version: 1.0.0
+
+
+
+Author: Travel Tec
+
+
+
+Author URI: https://traveltec.com.br
+
+
+
+License: GPLv2
+
+
+
+*/
+
+
+
+
+
+
+
+/*
+
+
+
+ * Plugin Update Checker
+
+
+
+ * 
+
+
+
+ * Note: If you're using the Composer autoloader, you don't need to explicitly require the library.
+
+
+
+ * @link https://github.com/YahnisElsts/plugin-update-checker
+
+
+
+ */
+
+ 
+
+ 
+require_once plugin_dir_path(__FILE__) . 'includes/tarifario-functions.php';
+
+
+
+
+ 
+
+
+
+
+
+
+ 
